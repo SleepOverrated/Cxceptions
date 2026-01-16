e055da5c04e58de711290e2482e25cbd7f658646
SleepOverrated <121642264+SleepOverrated@users.noreply.github.com>
2026-01-16 10:50:17 +0100

    Revert "feat: allow removal of location info"
    This reverts commit 15224f4512e933cbd86972dfbdeb50849d58d9dd.

--- cxeptions.h
+++ cxeptions.h
@@ -4,7 +4,6 @@
 #include <setjmp.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 #ifndef CXCEPTIONS_MESSAGE_BUF
@@ -13,35 +12,25 @@
 
 #ifndef CXCEPTIONS_MAX_TREE
 #define CXCEPTIONS_MAX_TREE 255
-#endif // !CXCEPTIONS_MAX_TREE
+#endif /* ifndef CXEPTIONS_MAX_TREE */
 
 typedef struct exception {
 	char* type;
 	char* message;
 
-#ifndef CXCEPTIONS_NO_LOCATION // Allow user to remove location info
 	char* file;
 	size_t line;
-#endif // !CXCEPTIONS_NO_LOCATION
 } exception_t;
 
 extern exception_t* cxceptions_current;
 extern jmp_buf cxceptions_jmpbuf[CXCEPTIONS_MAX_TREE];
+extern char has_catch[];
 extern int cxceptions_jmpbuf_index;
 
 void cxceptions_throw_exception(exception_t* exception);
 exception_t cxceptions_fmt_exception(char* type, char* format, ...);
 void cxceptions_handle_no_catch();
-
-#ifdef CXCEPTIONS_NO_LOCATION // Allow user to remove location info
-static inline void print_exception(exception_t* e) {
-	fprintf(stderr, "%s Exception: %s\n", e->type, e->message);
-}
-#else
-static inline void print_exception(exception_t* e) {
-	fprintf(stderr, "%s Exception: %s\n  at %s:%lu\n", e->type, e->message, e->file, e->line);
-}
-#endif
+void print_exception(exception_t* exception);
 
 #define CONCAT_IMPL(a, b) a##b
 #define CONCAT(a, b) CONCAT_IMPL(a, b)
@@ -53,26 +42,17 @@ static inline void print_exception(exception_t* e) {
 
 #define CATCH(var_name)                          \
 	char CONCAT(cxceptions_catch, __LINE__) = 0; \
-	for (exception_t* var_name = cxceptions_current; CONCAT(cxceptions_catch, __LINE__) == 0 && cxceptions_current != NULL; CONCAT(cxceptions_catch, __LINE__) = 1)
+	for (exception_t* var_name = cxceptions_current; CONCAT(cxceptions_catch, __LINE__) == 0 && cxceptions_current != NULL; CONCAT(cxceptions_catch, __LINE__) = 1, free(var_name->message))
 
 #define EXCEPTION(t, m, ...) cxceptions_fmt_exception(t, m, ##__VA_ARGS__)
 
 #define EXCEPTION_NO_FMT(t, m) \
 	(exception_t) { .type = t, .message = m }
 
-// Allow user to remove location info
-#ifdef CXCEPTIONS_NO_LOCATION
-
-#define THROW(exception) cxceptions_throw_exception(&exception)
-
-#else
-
 #define THROW(exception)                             \
 	exception_t CONCAT(throw, __LINE__) = exception; \
 	CONCAT(throw, __LINE__).line = __LINE__;         \
 	CONCAT(throw, __LINE__).file = __FILE__;         \
-	cxceptions_throw_exception(&CONCAT(throw, __LINE__))
-
-#endif // ! CXCEPTIONS_NO_LOCATION
+	throw_exception(&CONCAT(throw, __LINE__))
 
 #endif // !CXEPTIONS
